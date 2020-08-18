f19330fc2c44342d71fea37e5b1251b3a3e4a1bd
trossossp3 <21trossos@gmail.com>
2020-08-18 12:31:46 -0400

    did more code

--- firstProject/teaching/test.py
+++ firstProject/teaching/test.py
@@ -2,6 +2,18 @@ nameVariable = 10
 booleanVar = False
 s1 = "hello my name is trent"
 
-a = 10
-b = 20
+a = 11
+b = 10
 
+if a > b: #if true print 'hello' if it is false print 'goodbye'
+    print("hello")
+elif a==b:
+    print("i dont know im not creative")
+else:
+    print("goodbye")
+
+
+def name(name):
+    print(name)
+
+name("trent")
